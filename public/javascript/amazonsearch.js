121739f9f66309332fc6f97df8f6b5ea85a7941c
lfontanilla <lfontanilla@RENTAL002.vsi.digiport.us>
2013-09-11 14:43:02 -0500

    advancin in integrating knockout to the project

--- public/javascript/amazonsearch.js
+++ public/javascript/amazonsearch.js
@@ -1,1 +1,20 @@
-console.log("Stuff");
+$(function() {
+  var item = function(data) {
+    var self = this;
+    self.ASIN = ko.observable(data.UniqueIdentifier);
+    self.LowestNewPrice = ko.observable(data.LowestNewPrice);
+    self.LowestUsedPrice = ko.observable(data.LowestUsedPrice);
+    self.LowestRefurbishedPrice = ko.observable(data.LowestRefurbishedPrice);
+    self.CurrencyCode = ko.observable(data.CurrencyCode);
+    self.DetailPageURL = ko.observable(data.DetailPageURL);
+    self.SmallImageURL = ko.observable(data.SmallImageURL);
+  };
+
+  var viewModel = function() {
+    var self = this;
+    self.AmazonSearchResults = ko.observableArray([]);
+  };
+
+  var vm = new viewModel();
+  ko.applyBindings(vm);
+});
