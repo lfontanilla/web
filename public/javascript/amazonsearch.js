526efb47b5c0a69636258cb21973cc2ffa92d015
lfontanilla <lfontanilla@RENTAL002.vsi.digiport.us>
2013-09-12 12:05:49 -0500

    saves into an observable collection the search results

--- public/javascript/amazonsearch.js
+++ public/javascript/amazonsearch.js
@@ -12,15 +12,23 @@ $(function() {
 
   var viewModel = function() {
     var self = this;
-    console.log("searchresults");
-    var stuff = searchresults.replace(/&quot;/g,'"');
 
-    stuff = stuff.substring(1);
-    stuff = stuff.substring(0, stuff.length - 1);
-    stuff = JSON.parse(stuff);
-    console.log(stuff.Items);
-    
+    searchresults = searchresults.replace(/&quot;/g, '"');
+    searchresults = searchresults.substring(1);
+    searchresults = searchresults.substring(0, searchresults.length - 1);
+    searchresults = JSON.parse(searchresults);
+
     self.AmazonSearchResults = ko.observableArray([]);
+    var searchResultsMapped = [];
+    $.each(searchresults.Items.Item, function(key, value) {
+      var LowestNewPrice = value.OfferSummary.LowestNewPrice;
+      var LowestUsedPrice = value.OfferSummary.LowestUsedPrice;
+      var LowestRefurbishedPrice = value.OfferSummary.LowestRefurbishedPrice;
+      var element = new item({"ASIN": value.ASIN, "LowestNewPrice": LowestNewPrice ? (LowestNewPrice.Amount / 100) : -1, "LowestUsedPrice": LowestUsedPrice ? (LowestUsedPrice.Amount / 100) : -1, "LowestRefurbishedPrice": LowestRefurbishedPrice ? (LowestRefurbishedPrice.Amount / 100) : -1, "CurrencyCode": LowestNewPrice ? LowestNewPrice.CurrencyCode : LowestUsedPrice ? LowestUsedPrice.CurrencyCode : LowestRefurbishedPrice ? LowestRefurbishedPrice.CurrencyCode : "NA", "DetailPageURL": value.DetailPageURL, "SmallImageURL": value.SmallImage.URL});      
+      searchResultsMapped.push(element);
+    });
+    
+    self.AmazonSearchResults(searchResultsMapped);
   };
 
   var vm = new viewModel();
