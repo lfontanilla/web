79c41fe14000802ec632a080ecf9b37e55017874
lfontanilla <lfontanilla@RENTAL002.vsi.digiport.us>
2013-09-13 11:57:07 -0500

    filtering: done bitch!

--- public/javascript/amazonsearch.js
+++ public/javascript/amazonsearch.js
@@ -12,13 +12,8 @@ $(function() {
 
   var viewModel = function() {
     var self = this;
-
     self.searchterms = ko.observable(searchterms);
-//    with ( valueUpdate:'afterkeydown' ) in the UI it will listen to every keypress    
-//    self.searchterms.subscribe(function(newValue) {
-//      console.log(newValue);
-//    });
-
+    
     searchresults = searchresults.replace(/&quot;/g, '"');
     searchresults = searchresults.substring(1);
     searchresults = searchresults.substring(0, searchresults.length - 1);
@@ -36,6 +31,66 @@ $(function() {
 
     self.AmazonSearchResults(searchResultsMapped);
 
+    //filter and search vars    
+    self.filterOptions = ko.observableArray([{value: "ASIN", Name: "ASIN"},
+      {value: "LowestNewPrice", Name: "Lowest New Price"},
+      {value: "LowestUsedPrice", Name: "Lowest Used Price"},
+      {value: "LowestRefurbishedPrice", Name: "Lowest Refurbished Price"},
+      {value: "CurrencyCode", Name: "Currency Code"}]);
+    
+    self.selectedOption = ko.observable("");
+    self.filterValue = ko.observable("");
+
+    self.SizeOfAll = ko.observable(0);
+    self.PageSize = ko.observable(4);
+    self.PageIndex = ko.observable(0);
+    
+    self.filterValue.subscribe(function(oldValue) {
+      self.PageIndex(0);      
+    }, null, "beforeChange");
+    
+    self.PreviousPage = function() {
+      self.PageIndex(self.PageIndex() - 1);
+    };
+
+    self.NextPage = function() {
+      self.PageIndex(self.PageIndex() + 1);
+    };
+
+    self.PagedRows = ko.dependentObservable(function() {
+      var size = self.PageSize();
+      var start = self.PageIndex() * size;
+      if (self.filterValue()) {
+        start = self.PageIndex() * size;
+        var stuff = filter();
+        self.SizeOfAll(stuff.length);
+        return stuff.slice(start, size + start);
+      }
+      else {
+        self.SizeOfAll(self.AmazonSearchResults().length);
+        return self.AmazonSearchResults.slice(start, size + start);
+      }
+    });
+
+    function filter() {
+      return ko.utils.arrayFilter(self.AmazonSearchResults(), function(item) {
+        if (self.selectedOption().value == "ASIN")
+          return item.ASIN().toLowerCase().indexOf(self.filterValue().toLowerCase()) != -1;
+        if (self.selectedOption().value == "LowestNewPrice")
+          return item.LowestNewPrice().toString().toLowerCase().indexOf(self.filterValue().toLowerCase()) != -1;
+        if (self.selectedOption().value == "LowestUsedPrice")
+          return item.LowestUsedPrice().toString().toLowerCase().indexOf(self.filterValue().toLowerCase()) != -1;
+        if (self.selectedOption().value == "LowestRefurbishedPrice")
+          return item.LowestRefurbishedPrice().toString().toLowerCase().indexOf(self.filterValue().toLowerCase()) != -1;
+        if (self.selectedOption().value == "CurrencyCode")
+          return item.CurrencyCode().toLowerCase().indexOf(self.filterValue().toLowerCase()) != -1;
+      });
+    }
+
+    self.MaxPageIndex = ko.dependentObservable(function() {
+      return Math.ceil(self.SizeOfAll() / self.PageSize()) - 1;
+    });
+    
     self.checkForEnter = function(data, event) {
       try {
         if (event.which == 13) {
@@ -53,11 +108,11 @@ $(function() {
       AmazonSearch();
     };
 
-    function AmazonSearch() {      
+    function AmazonSearch() {
       window.location = '/amazonsearch/' + self.searchterms();
     }
   };
-
+  
   var vm = new viewModel();
   ko.applyBindings(vm);
 });
